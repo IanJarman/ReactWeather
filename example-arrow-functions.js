574f94bcf5d4106f1576c7412b275e7cd3f8db22
Ian Jarman <ian@sailing-fast.com>
2016-07-05 16:05:03 +0100

    Initial commit

---
+++ example-arrow-functions.js
@@ -0,0 +1,12 @@
+// var names = ['Ian', 'Liz', 'Stephen'];
+//
+// names.forEach(function (name) {
+//   console.log('forEach', name);
+//
+// });
+//
+// names.forEach((name) =>{
+//   console.log('arrowFunc', name);
+// });
+//
+// names.forEach()
